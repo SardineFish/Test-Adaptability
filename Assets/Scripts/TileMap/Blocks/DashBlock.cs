e3bef7c91a326abd855eedf2ebe6a9c8f5a6837c
SardineFish <Sardine@live.cn>
2019-10-31 20:08:04 +0800

    Add dash block.

---
+++ Assets/Scripts/TileMap/Blocks/DashBlock.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Project.Blocks
+{
+    [CreateAssetMenu(fileName ="DashBlock",menuName ="Blocks/Dash")]
+    public class DashBlock : Block
+    {
+        public float Speed;
+        public override IEnumerator ProcessPlayerContacted(GameEntity player, Vector2 point, Vector2 normal)
+        {
+            var motionController = player.GetComponent<Controller.PlayerMotionController>();
+            var playerController = player.GetComponent<Controller.PlayerController>();
+            var input = player.GetComponent<Controller.PlayerInput>();
+            if (Vector2.Dot(normal, DirectionVector) < -0.9f)
+                yield break;
+            if(BlockDirection == BlockDirection.Down || BlockDirection == BlockDirection.Up)
+            {
+                motionController.YControl = Controller.ControlType.Velocity;
+                motionController.Move(Speed * DirectionVector);
+                while(playerController.IsContactedWith(this))
+                {
+                    motionController.Move(Speed * DirectionVector);
+                    if (input.CachedJumpPress)
+                    {
+                        motionController.YControl = Controller.ControlType.Ignored;
+                        yield break;
+                    }
+                    yield return new WaitForFixedUpdate();
+                }
+                motionController.YControl = Controller.ControlType.Ignored;
+                yield break;
+            }
+            else if (BlockDirection == BlockDirection.Left || BlockDirection == BlockDirection.Right)
+            {
+                motionController.XControl = Controller.ControlType.Velocity;
+                motionController.Move(Speed * DirectionVector);
+                while (playerController.IsContactedWith(this))
+                {
+                    motionController.Move(Speed * DirectionVector);
+                    if (input.CachedJumpPress)
+                    {
+                        motionController.YControl = Controller.ControlType.Ignored;
+                        motionController.XControl = Controller.ControlType.Velocity;
+                        yield break;
+                    }
+                    yield return new WaitForFixedUpdate();
+                }
+                yield break;
+            }
+        }
+    }
+
+}
